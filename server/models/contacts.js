60ca1ac177df4c8d67da0fa559c32aa95a04748e
AdilHussain919 <adilhuss919@gmail.com>
2017-03-11 13:52:09 -0500

    improved file structure and added auth and contacts views and db models

---
+++ server/models/contacts.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+
+// create a model class
+let contactsSchema = mongoose.Schema({
+    name: String,
+    rating: Number,
+    cost: Number
+},
+{
+  collection: "contacts"
+});
+
+module.exports = mongoose.model('contacts', gamesSchema);
